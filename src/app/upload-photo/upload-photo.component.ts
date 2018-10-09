06d44c4bf960d57ba3265c2df52f3f527eabd9e0
hercat <991146900@qq.com>
2018-10-09 10:50:36 +0800

    upload photo file chose

--- src/app/upload-photo/upload-photo.component.ts
+++ src/app/upload-photo/upload-photo.component.ts
@@ -1,17 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit } from '@angular/core';
 
 import $ from 'jquery';
-import {d} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-upload-photo',
   templateUrl: './upload-photo.component.html',
   styleUrls: ['./upload-photo.component.scss']
 })
-export class UploadPhotoComponent implements OnInit {
+export class UploadPhotoComponent implements OnInit, AfterViewInit {
   imageCount: number;
   uploadImageData: FormData;
-  addImageDom = '<i class="fa fa-plus" style="font-size: 30px"></i>';
+  addImageDom = `<div class="add-photo-btn waves-ripple" mdbWavesEffect>
+                    <i class="fa fa-plus" style="font-size: 30px"></i>
+                 </div>`;
 
   constructor() {
     this.imageCount = 0;
@@ -19,6 +20,13 @@ export class UploadPhotoComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const that = this;
+    $('.add-photo-btn').click(function () {
+      that.choseImage();
+    });
   }
 
   choseImage() {
@@ -31,30 +39,49 @@ export class UploadPhotoComponent implements OnInit {
     if (this.imageCount + lengthOfImages > 9) {
       // show alert info
       $('.alert')[0].classList.add('show');
+      $('.close').click(function () {
+        $('.alert')[0].classList.remove('show');
+      });
     } else {
-      for (let index = this.imageCount; index < lengthOfImages; index++) {
+      for (let index = 0; index < lengthOfImages; index++) {
         this.uploadImageData.append('photos', (imageInput as any).files[index]);
         const fileReader = new FileReader();
         fileReader.readAsDataURL((imageInput as any).files[index]);
-        this.imageReadHandler(index + 1, fileReader);
+        this.imageCount++;
+        this.imageReadHandler(this.imageCount, fileReader);
       }
-      this.imageCount += lengthOfImages;
+      if (this.imageCount < 9) {
+        const id = this.imageCount + 1;
+        console.log(id);
+        const dom = $(this.addImageDom);
+        const that = this;
+        dom.click(function () {
+          that.choseImage();
+        });
+        $(`#${id}`).append(dom);
+      }
     }
   }
 
   imageReadHandler(index: Number, fileReader: FileReader) {
-
-    console.log(index);
     fileReader.addEventListener('load', function () {
       const innerHtml = $(`<img src="${fileReader.result}" class="img-fluid"/>
-              <div class="mask white-text info-container rgba-black-strong">
-                <span>enmmm...</span>
-              </div>`);
+<div class="mask white-text info-container rgba-black-strong">
+  <div class="input-group input-group-sm mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text" id="photo-name">照片名称</span>
+    </div>
+    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="photo-name">
+  </div>
+  <div class="input-group input-group-sm mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text" id="photo-desc">照片信息</span>
+    </div>
+    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="photo-desc">
+  </div>
+</div>`);
       const div = $(`#${index}`);
       div.html('');
-      div.click(function () {
-        console.log('hh');
-      })
       div.append(innerHtml);
     });
   }
